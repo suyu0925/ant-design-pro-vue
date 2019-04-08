fa119b8c1665f146c1660a42d97c62e49e684010
suyu0925 <suyu0925@gmail.com>
2019-04-08 21:48:10 +0800

    use vue-router

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const constantRouterMap = [
+  {
+    path: '/login',
+    component: () => import(/* webpackChunkName: "user" */ '../views/user/Login')
+  },
+  {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "fail" */ '../views/exception/404'),
+    hidden: true
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+export default new Router({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
