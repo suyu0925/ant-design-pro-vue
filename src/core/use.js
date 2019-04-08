7822795f399dd20c5afd5c9bd77511a627943299
suyu0925 <suyu0925@gmail.com>
2019-04-08 21:05:47 +0800

    use ant-design-vue, write the frist page: login

---
+++ src/core/use.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+// base library
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './core/use'
+
 Vue.config.productionTip = false
 
 new Vue({
